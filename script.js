0750a60fea73ca03fcc23e0987d0abb4e98d5eaf
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-02-22 08:38:21 +0000

    Adds logic for basic operations

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = (a , b) => {
+    const sum = a + b;
+    return sum;
+}
+
+
+const subtract = (a , b) => {
+    const difference = a - b;
+    return difference;
+}
+
+
+const multiply = (a , b) => {
+    const product = a * b;
+    return product;
+}
+
+
+const divide = (a , b) => {
+    const quotient = a / b;
+    return quotient;
+}
