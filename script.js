afb50e0506eb9e49d711c78238976c609a49772c
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-02-29 02:16:26 +0000

    Add the operate function

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ const divide = (a , b) => {
     const quotient = a / b;
     return quotient;
 }
+
+
+const firstNumber = '';
+const operator = '';
+const secondNumber = '';
+
+const operate = (operator,firstNumber, lastNumber) => {
+   
+    if (operator == 'add') {
+       const value = add(firstNumber , lastNumber);
+       return value;
+    } 
+     else if (operator == 'subtract') {
+        const value = subtract(firstNumber , lastNumber);
+        return value;
+     }
+     else if (operator == 'multiply') {
+        const value = multiply(firstNumber , lastNumber);
+        return value;
+     }
+     else if (operator == 'divide') {
+        const value = divide(firstNumber , lastNumber);
+        return value;
+     }
+
+     
+}
+console.log(operate('multiply', 4 , 2));
+
