208d18896b8f34548222c3d9cfd68ca294d909d6
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-03-03 13:56:07 +0000

    Add logic for populating display

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const buttons = document.querySelector('.buttons');
-const display = document.querySelector('.display');
-const equal = document.querySelector('#equal');
+let firstNumber = 0;
+let secondNumber = 0;
+let operant = '';
+let previousNumber = '';
+let result = 0;
+const prevValue = document.querySelector('.prev-value');
+const currentValue = document.querySelector('.current-value');
+const deleteNumber = document.querySelector('.delete');
+const clear = document.querySelector('.clear');
+const numbers = document.querySelectorAll('.btn');
+const operators = document.querySelectorAll('.operant');
+const equal = document.querySelectorAll('.equal');
 
 const add = (a , b) => {
     const sum = a + b;
@@ -26,9 +35,6 @@ const divide = (a , b) => {
 }
 
 
-const firstNumber = '';
-const operator = '';
-const secondNumber = '';
 
 const operate = (operator,firstNumber, lastNumber) => {
    
@@ -52,11 +58,55 @@ const operate = (operator,firstNumber, lastNumber) => {
      
 }
 
-
+/*
 buttons.addEventListener("click", (event) => {
    if(event.target.matches('span')){
     const displayValue = event.target.textContent;
     display.textContent += displayValue;
     display.style.fontSize = '25px';
    }
+});*/
+
+/*
+numbers.forEach(number =>{
+   number.addEventListener('click', () =>{
+      const value = number.textContent;
+      prevValue.textContent += value;
+   })
+});*/
+let operatorClicked = false;
+
+numbers.forEach(number =>{
+   number.addEventListener('click', () => {
+      const value = number.textContent;
+      prevValue.textContent += value;
+      
+      if(!operatorClicked){
+         firstNumber = prevValue.textContent;
+      }
+
+      else{
+         secondNumber = prevValue.textContent;
+      }
+      
+      operators.forEach(operator => {
+         operator.disabled = false;
+      });
+   });
+});
+
+operators.forEach(operator => {
+   operator.addEventListener('click', ()=>{
+      const operatorValue = operator.textContent;
+      prevValue.textContent += operatorValue;
+      operant = prevValue.textContent;
+      
+      operatorClicked = true;
+      
+      operators.forEach(operator => {
+         operator.disabled = true;
+      });
+     
+   });
+   
 });
